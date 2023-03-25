5508267a3b33c2b17a429e61d648364f860ac2cc
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-25 19:14:22 +1300

    Fetched GIFs from Giphy and added a few data like'.images and .url into src to avoid undefined result

--- Submission/script.js
+++ Submission/script.js
@@ -1,8 +1,15 @@
 const renderRandomGif = async () => {
   // TODO: Make a fetch request to the Giphy API to get a random GIF
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/random?api_key=GZPXeabGycACym7d9QoLCSwWUzMPINMc&tag=&rating=g"
+    );
+  const gif = await response.json();
+   
+  console.log(gif.data.images.fixed_height_downsampled.url);
 
+  
   // TODO: Update the 'src' variable below to be a URL to a GIF from the Giphy API
-  const src = "";
+  const src = gif.data.images.fixed_height_downsampled.url;
 
   // Update the existing <img> tag with the new src
   const imgEl = document.querySelector("#random-gif");
